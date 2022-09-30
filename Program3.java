6c6ab79d04e6f88f52877fc0a0662430adf9fd0c
My-Life1997 <77240123+My-Life1997@users.noreply.github.com>
2022-09-30 09:44:44 -0400

    Add files via upload

--- Program3.java
+++ Program3.java
@@ -1,19 +1,24 @@
-package Java;
+package TestNG;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class Program3 {
-    public static void main(String[] args) throws InterruptedException {
+    @Parameters({"Email","Pass"})
+    @Test
+    public void sample(String Email,String Pass) throws Throwable
+    {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\i732486\\Selenium jar\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        driver.manage().window().maximize();
-        driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
-        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("HELLO");
-        driver.findElement(By.cssSelector("input[id='continue']")).click();
+
+        driver.get("https://www.instagram.com/");
         Thread.sleep(3000);
-        driver.close();
+
+        driver.findElement(By.name ("username")).sendKeys(Email);
+        driver.findElement(By.name ("password")).sendKeys(Pass);
+
     }
 }
